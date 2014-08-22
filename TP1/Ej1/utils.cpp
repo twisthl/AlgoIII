cd726e752e58054ecce913a7154da2a1cd6ddc93
twisthl <twizt.hl@gmail.com>
2014-08-22 03:00:42 -0300

    ej1

--- TP1/Ej1/utils.cpp
+++ TP1/Ej1/utils.cpp
@@ -7,13 +7,40 @@ LJumps resolver(int cantTablones, int saltoMaximo, LTablonesEstado& tablones){
 
 
 //	vector<int>::iterator lastBoard;
-	int ultimoTablon;
-	int ultimoTablonValido;
+//	int ultimoTablon;
+//	int ultimoTablonValido;
 
+	LSaltos saltos* = new LSaltos();
+	
+	std::vector<int>::iterator ultimoTablon = tablones.begin();
+	
+	int contadorSaltoMaximo = 0;
+	
+	//inserto el primer tablon
+//	saltos->push_back(1);
+	
+	
+//	for(ultimoTablon; ultimoTablon != tablones.end(); ultimoTablon++){
+	for(int contadorNroTablon = 0; contadorNroTablon < cantTablones; contadorNroTablon++){
 
-	for(int i=0; i<cantTablones; i++){
-
+//		contadorNroTablon++;
+		contadorSaltoMaximo++;
+		
+		//Si hay un tablon en ese lugar actualizo tablonASaltar
+		//Es posible que tenga que agregar un bool para chequear que haya podido hacer algun salto el cual se setea a true aca.
+		if(tablones[contadorNroTablon] == 1){
+			tablonASaltar = contadorNroTablon;
+		}
+		
+		//Si llegue al limite de salto agrego (Y pude hacer un salto) agrego el tablon al que voy a saltar a la lista de saltos y reinicio los contadores
+		if(contadorSaltoMaximo == saltoMaximo){
+			
+			saltos->push_back(contadorNroTablon);
+			contadorSaltoMaximo = 0;
+		}
 	}
+	
+	return saltos;//Tambien devolver si se pudo llegar al final con saltos.
 
 
 /*	for(int i=0; i < saltoMaximo; i++){
