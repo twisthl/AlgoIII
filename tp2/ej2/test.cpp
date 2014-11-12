d85a7797346052f7c990139afad2cf2be1717682
Augusto <jam@TOSHIBA.(none)>
2014-11-12 00:24:23 -0300

    Estan los test

--- tp2/ej2/test.cpp
+++ tp2/ej2/test.cpp
@@ -20,13 +20,13 @@ int main(int argc, char *argv[]){
 		ofstream fileTiempos("tiemposN100.out");
 
 		// Tests aleatorios	
-		for(int i=1;i<150;i++){				
-			srand(seed + i);
+		//for(int i=1;i<150;i++){				
+			srand(seed );
 
 			clock_t t = clock();
 
 			Caballos caballos;
-			for(int j=0; j<i; j++){
+			for(int j=0; j<150; j++){
 				int x = (rand() % n) + 1;
 				int y = (rand() % n) + 1;
 				Casillero casilla(x,y);
@@ -35,13 +35,13 @@ int main(int argc, char *argv[]){
 
 			MatrizDeNodos* matriz = modelar(n);
 
-			Nodo solucion = resolver(*matriz, n, caballos, i);
+			Nodo solucion = resolver(*matriz, n, caballos, 150);
 
 			t = clock() - t;
 
 			fileTiempos << i << "\t" << t << endl;
 			cout << i << "\t" << t << endl;
-		}
+		//}
 	
 	return 0;
 	}
