2fe4fd59cf78db5e613331210a02a9ba137064ca
Pato <patriciocapanna@gmail.com>
2014-12-13 22:31:08 -0300

    parser y opciones ahora si

---
+++ TP3/src/opciones.h
@@ -0,0 +1,47 @@
+/**
+ * Class Opciones: Mini clase con opciones del programa
+ */
+#ifndef _OPCIONES_H_
+#define _OPCIONES_H_
+
+#include <iostream>
+
+using namespace std;
+
+#define EXACTO 0
+#define GREED 1
+#define BUSQUEDA_LOCAL 2
+#define GRASP 3
+
+class Opciones{
+	public:
+		Opciones();
+		~Opciones(){};
+
+		// Carga los argumentos que entran por parametro.
+		bool cargarArgumentos(int argc, char* argv[]);
+		// Imprime las acciones que va a realizar el programa.
+		void imprimirAcciones();
+		// Realiza comprobaciones sobre los argumentos.
+		bool consistente();
+		// Imprime un mensaje indicando la falta de argumento.
+		void faltaArgumento(char* opcion);
+		// Muestra el Help.
+		void mostrarUso();
+		// Mini metodo que imprime el titulo del tp.
+		void imprimirTitulo();
+
+		int exercise;
+		bool help;
+		bool archivo;
+		bool poda_habilitada;
+		bool mostrar_info;
+
+		string archivo_entrada;
+		string archivo_salida;
+
+		// Nombre del programa
+		const char* nombre_programa;
+};
+
+#endif
---
+++ TP3/src/parser.h
@@ -0,0 +1,29 @@
+#ifndef _PARSER_H_
+#define _PARSER_H_
+
+#include "tipos.h"
+#include "opciones.h"
+#include "exacto.h"
+#include "greed.h"
+
+using namespace std;
+
+
+class Parser {
+	public:
+		Parser(Opciones opt);
+		~Parser(){};
+		void resolver();
+		void guardarTiempoEjecucion(int n, double time);
+		void mostrarSolucion();
+
+	private:
+		int exercise;
+		vector<int> solucion;
+		string archivo_entrada;
+		string archivo_salida;
+		bool mostrar_info;
+		bool poda_habilitada;
+};
+
+#endif
