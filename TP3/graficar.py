ad90761fd13acc061f8f4ff1463246c3dc0be613
programador <programador@programador-hp.(none)>
2014-12-17 12:10:07 -0300

    asdasdasd

--- TP3/graficar.py
+++ TP3/graficar.py
@@ -8,82 +8,78 @@ def main(argv):
 
 def construirRandomInput():
     from random import randint
-    for k in range(2, 6):
-    	print(str(k))
-    	for n in range(5, 15):
-    		print(n)
-    		m = int(((n * (n-1))/2)*0.15)
+    for k in range(2, 7):
+        for n in range(5, 16):
+            m = int(((n * (n-1))/2)*0.15)
+            
+            nombre = 'N' + str(n) + 'K' + str(k) + 'M15.grafo'
 
-    		print(str(m))
-    		
-    		nombre = 'N' + str(n) + 'K' + str(k) + 'M15.grafo'
+            s = open(str('recursos/' + nombre), 'wb+')
 
-    		s = open(str('recursos/' + nombre), 'wb+')
+            s.write(str(n) + ' ' + str(m) + ' ' + str(k) + '\n')
 
-    		s.write(str(n) + ' ' + str(m) + ' ' + str(k) + '\n')
+            aristas = {}
 
-    		aristas = {}
+            for i in range(0, m):
+                origen = randint(0, (n-2))
+                destino = randint(1, (n-1))
+                while (destino<=origen):
+                    destino = randint(1, (n-1))
 
-    		for i in range(0, m):
-    			origen = randint(0, (n-1))
-		        destino = randint(1, n)
-		        while (destino<origen):
-			        destino = randint(1, n)
+                while ((str(str(origen)+str(destino))) in aristas):
+                    origen = randint(0, (n-2))
+                    destino = randint(1, (n-1))
+                    while (destino<=origen):
+                        destino = randint(1, (n-1))
 
-				while ((str(str(origen)+str(destino))) in aristas):
-					origen = randint(0, (n-1))
-		      		destino = randint(1, n)
-		        	while (destino<origen):
-			        	destino = randint(1, n)
+                aristas[str(str(origen)+str(destino))] = 1
 
-				aristas[str(str(origen)+str(destino))] = 1
+                w = randint(1, 999)
 
-				w = randint(1, 999)
+                s.write(str(origen) + ' ' + str(destino) + ' '+ str(w) + '\n')
 
-				s.write(str(origen) + ' ' + str(destino) + ' '+ str(w) + '\n')
+            s.close()
 
-			s.close()
+            m = int(((n * (n-1))/2)/2)
+            nombre = 'N' + str(n) + 'K' + str(k) + 'M50.grafo'
 
-			m = int(((n * (n-1))/2)/2)
-    		nombre = 'N' + str(n) + 'K' + str(k) + 'M50.grafo'
+            t = open(str('recursos/' + nombre), 'wb+')
 
-    		t = open(str('recursos/' + nombre), 'wb+')
+            t.write(str(n) + ' ' + str(m) + ' ' + str(k) + '\n')
 
-    		t.write(str(n) + ' ' + str(m) + ' ' + str(k) + '\n')
+            aristas = {}
 
-    		aristas = {}
+            for i in range(0, m):
+                
+                origen = randint(0, (n-2))
+                destino = randint(1, (n-1))
+                while (destino<=origen):
+                    destino = randint(1, (n-1))
 
-    		for i in range(0, m):
-    			
-    			origen = randint(0, (n-1))
-		        destino = randint(1, n)
-		        while (destino<origen):
-			        destino = randint(1, n)
+                while ((str(str(origen)+str(destino))) in aristas):
+                    origen = randint(0, (n-2))
+                    destino = randint(1, (n-1))
+                    while (destino<=origen):
+                        destino = randint(1, (n-1))
 
-				while ((str(str(origen)+str(destino))) in aristas):
-					origen = randint(0, (n-1))
-		      		destino = randint(1, n)
-		        	while (destino<origen):
-			        	destino = randint(1, n)
+                aristas[str(str(origen)+str(destino))] = 1
 
-				aristas[str(str(origen)+str(destino))] = 1
+                w = randint(1, 999)
 
-				w = randint(1, 999)
+                t.write(str(origen) + ' ' + str(destino) + ' '+ str(w) + '\n')
+            t.close()
 
-				t.write(str(origen) + ' ' + str(destino) + ' '+ str(w) + '\n')
-			t.close()
+            m = (n * (n-1))/2
+            nombre = 'N' + str(n) + 'K' + str(k) + 'M100.grafo'
 
-			m = (n * (n-1))/2
-    		nombre = 'N' + str(n) + 'K' + str(k) + 'M100.grafo'
+            r = open(str('recursos/' + nombre), 'wb+')
 
-    		r = open(str('recursos/' + nombre), 'wb+')
+            r.write(str(n) + ' ' + str(m) + ' ' + str(k) + '\n')
 
-    		r.write(str(n) + ' ' + str(m) + ' ' + str(k) + '\n')
-
-    		for i in range(0, (n-1)):
-	    		for j in range((i+1), n):
-	    			w = randint(1, 999)
-	    			r.write(str(i) + ' ' + str(j) + ' ' + str(w) + '\n')
-			r.close()
+            for i in range(0, (n-1)):
+                for j in range((i+1), n):
+                    w = randint(1, 999)
+                    r.write(str(i) + ' ' + str(j) + ' ' + str(w) + '\n')
+            r.close()
 
 if __name__=="__main__":main(sys.argv[1:])
